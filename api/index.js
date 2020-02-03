edf9c12f5862921b0d81efdd17d578d9de891872
elaineshimabukuro <laineyoshizato@gmail.com>
2020-02-03 12:20:12 -0300

    deleted node_modules

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000
 app.use('/api/products',productRoutes)
 
 app.get('*', (req, res) => res.status(200).send({
-   message: 'Esta é a API do noss Restaurante.'
+   message: 'Esta é a API do nosso Restaurante.'
 }))
 
 app.listen(port, () => {
