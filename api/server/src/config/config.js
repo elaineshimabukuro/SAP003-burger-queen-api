10d9b4ebf6d4c2db185071c35a90323891d47bf1
elaineshimabukuro <laineyoshizato@gmail.com>
2020-02-03 12:04:43 -0300

    alterações do heroku

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 module.exports = {
   "development": {
     "username": "postgres",
@@ -16,11 +17,14 @@ module.exports = {
    
   },
   "production": {
-    "username": "postgres",
-    "password": null,
-    "database": "database_production",
-    "host": "db",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": true,
+    }
    
   }
 }
