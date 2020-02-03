c9b146c7f44b1cf0e3286f161bf8b45dbdc0c7df
elaineshimabukuro <laineyoshizato@gmail.com>
2020-02-03 11:49:44 -0300

    crud products ok

--- api/contollers/productController.js
+++ api/contollers/productController.js
@@ -3,38 +3,47 @@ import models from '../server/src/models/index'
 const getAll = async (req, res) => {
     const products = await models.Products.findAll({ raw: true });
     res.json(products);
-}
+};
 
 const addProduct = async (req, res) => {
     if (req.body.name && req.body.price && req.body.is_breakfast) {
         const newProduct = await models.Products.create(req.body);
         res.json(newProduct);
     } else {
-        res.json({message:'complete name;price and if is breakfast'})
+        res.json({ message: 'complete name;price and if is breakfast' })
     }
-}
+};
 
 const getProduct = async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     const selectedProduct = await models.Products.findOne({ where: { id: id } });
     res.json(selectedProduct);
-}
+};
 
 const updatedProduct = async (req, res) => {
-    const modificatedProduct = await models.Products.update(req.body);
-    res.json(modificatedProduct);
-}
+    const { id } = req.params
+    const productToModificate = await models.Products.findOne({ where: { id: id } })
+    if(productToModificate){
+        const modificatedProduct = await models.Products.update({ where: {id: id} })
+        res.json(modificatedProduct);
+    }
+    
+};
 
 const deleteProduct = async (req, res) => {
-    const deletedProduct = await models.Products.destroy(req.body);
-    res.json(deletedProduct);
-}
+    const { id } = req.params
+    const productToDelete = await models.Products.findOne({ where: { id: id } })
+    if (productToDelete) {
+        const deletedProduct = await models.Products.destroy({ where: { id: id } })
+        res.json(deletedProduct);
+    }
+};
 
-export default {
-    getAll,
-    addProduct,
-    getProduct,
-    updatedProduct,
-    deleteProduct
-}
+    export default {
+        getAll,
+        addProduct,
+        getProduct,
+        updatedProduct,
+        deleteProduct,
+    }
 
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -5,6 +5,6 @@ const router = Router()
 router.get('/', productController.getAll)
 router.post('/', productController.addProduct)
 router.get('/:id', productController.getProduct)
-// router.put('/:id', productController.updatedProduct)
-// router.delete('/:id', productController.deleteProduct)
+router.put('/:id', productController.updatedProduct)
+router.delete('/:id', productController.deleteProduct)
 export default router
